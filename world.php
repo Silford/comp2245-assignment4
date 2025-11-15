fe9d0688c8ab5d70c97a724606f484a521b44e7d
Silford <silford.moore@my.fiveislands.uwi.edu>
2025-11-15 18:31:41 -0400

    Completed Exercise 4

--- world.php
+++ world.php
@@ -9,19 +9,41 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 if (isset($_GET["country"]) && $_GET["country"] !== "") {
     $country = $_GET["country"];
 
-    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+    // Use LIKE for partial search
+    $stmt = $conn->prepare("
+        SELECT name, continent, independence_year, head_of_state
+        FROM countries
+        WHERE name LIKE :country
+    ");
+
     $search = "%$country%";
     $stmt->bindParam(':country', $search, PDO::PARAM_STR);
     $stmt->execute();
 } else {
-    $stmt = $conn->query('SELECT * FROM countries');
+    $stmt = $conn->query("
+        SELECT name, continent, independence_year, head_of_state
+        FROM countries
+    ");
 }
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+// Build table
+echo "<table border='1' cellpadding='6' cellspacing='0'>
+        <tr>
+            <th>Country Name</th>
+            <th>Continent</th>
+            <th>Independence Year</th>
+            <th>Head of State</th>
+        </tr>";
+
+foreach ($results as $row) {
+    echo "<tr>
+            <td>{$row['name']}</td>
+            <td>{$row['continent']}</td>
+            <td>{$row['independence_year']}</td>
+            <td>{$row['head_of_state']}</td>
+          </tr>";
+}
+
+echo "</table>";
